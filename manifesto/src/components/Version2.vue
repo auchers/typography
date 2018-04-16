96fd49d7d2520bc16f574a2c5f45e6ef434a10c8
serrau <aucher.serr@gmail.com>
2018-04-16 15:37:50 -0400

    draft of version2

--- manifesto/src/components/Version2.vue
+++ manifesto/src/components/Version2.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="wrapper">
-    <p>{{title}}</p>
+    <p class="title">{{title}}</p>
+    <div class="grid">
+        <div class="cell" v-for="line in text">
+          <div class="text">
+            <div class="main">{{line.main}}</div>
+            <div class="sub">{{line.sub}}</div>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Version2',
-    props: ['title', 'text'],
+    props: ['title', 'author', 'text'],
     data () {
       return {
         msg: 'This is Version 2'
@@ -16,11 +24,41 @@
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .title{
+    font-size: 3em;
+    height: 100vh;
+  }
+
+  .main{
+    font-size: 4em;
+    letter-spacing: -1px;
+    font-kerning: normal;
+    /*line-height: 95%;*/
+  }
+
+  .sub{
+  }
+
+  .text{
+    position: absolute;
+    bottom: 2em;
+  }
+
   .wrapper{
-    font-family: "Typewriter Elite MT Std", serif;
+    font-family: "Typewriter MT Std", serif;
     font-weight: bolder;
+  }
+
+  .cell{
+    width: 90vw;
+    margin: 2em;
+    position: relative;
+  }
+
+  .grid{
+    display: grid;
+    grid-template:  90vh / repeat(18,130vw);
   }
 
 </style>
